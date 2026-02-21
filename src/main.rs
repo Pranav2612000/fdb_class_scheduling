88ccc08494837a85693e2332a612c29e33762d88
Pranav Joglekar <pranav2000joglekar@gmail.com>
2026-02-21 18:30:29 +0530

    chore: add tokio support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use foundationdb::api::FdbApiBuilder;
+
+#[tokio::main]
+async fn main() {
+    println!("Starting test fdb app");
+
+    let fdb_builder = FdbApiBuilder::default();
+    let fdb_builder = fdb_builder.set_runtime_version(730);
+
+    let network_builder = fdb_builder
+        .build()
+        .expect("should be able to build from fdb builder");
+    let network = unsafe { network_builder.boot() };
+
+    // Perform fdb operations
+
+    drop(network);
+    println!("Test fdb example completed");
 }
